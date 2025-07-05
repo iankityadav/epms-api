5ff9d416e3d569613031254caccb2de6893d9104
Ankit Yadav <64765952+iankityadav@users.noreply.github.com>
2025-07-05 18:04:22 +0530

    initial commit

---
+++ src/test/java/com/ankit/epms/EpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.epms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
